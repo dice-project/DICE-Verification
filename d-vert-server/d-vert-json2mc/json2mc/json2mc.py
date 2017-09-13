51a48f637f8327b184d532f125dcbe980e77d5a1
Francesco Marconi <franco.maroni@gmail.com>
2017-09-13 03:22:30 +0200

    Update versions to 0.3.0

--- d-vert-server/d-vert-json2mc/json2mc/json2mc.py
+++ d-vert-server/d-vert-json2mc/json2mc/json2mc.py
@@ -35,9 +35,9 @@ import config as cfg
 from factory_methods import DiaVerificationFactory
 
 __all__ = []
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 __date__ = '2015-11-10'
-__updated__ = '2017-07-20'
+__updated__ = '2017-09-13'
 
 DEBUG = 0
 TESTRUN = 0
